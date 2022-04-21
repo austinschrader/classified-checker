6a2632f4d13791dcf503b402bb3d19e01bf8cdb9
Austin Schrader <austin.jschrader@gmail.com>
2022-04-21 03:13:28 -0700

    Add logic to check emails

--- src/components/ClassifiedTool.js
+++ src/components/ClassifiedTool.js
@@ -6,6 +6,7 @@ import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import Title from "./Title";
+import Typography from "@mui/material/Typography";
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
@@ -13,16 +14,21 @@ const checkedIcon = <CheckBoxIcon fontSize="small" />;
 const filter = createFilterOptions();
 
 const ClassifiedTool = () => {
-  const handleSubmit = () => console.log(selectedOptions);
-  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState(defaultWords);
+  const [emailText, setEmailText] = useState("");
+  const [result, setResult] = useState(false);
 
-  const handleChange = (event, value) => setSelectedOptions(value);
+  const handleAutocompleteChange = (event, value) => setSelectedOptions(value);
+  const handleEmailChange = (event) => setEmailText(event.target.value);
+  const handleSubmit = () =>
+    setResult(isClassified(emailText, selectedOptions));
 
   return (
     <React.Fragment>
       <Title>New Scan</Title>
       <TextField
         sx={{ m: 2, width: "25ch" }}
+        onChange={handleEmailChange}
         InputProps={{
           endAdornment: (
             <Button
@@ -35,44 +41,33 @@ const ClassifiedTool = () => {
             </Button>
           ),
         }}
-        label="Email to Scan"
-        style={{ width: "90%" }}
+        label="Email Text to Scan"
+        style={{ width: "96%" }}
       />
       <Autocomplete
         multiple
         freeSolo
-        style={{ width: "90%", marginLeft: "15px" }}
+        style={{ width: "96%", marginLeft: "15px" }}
         size="small"
         id="classified-words-list"
-        onChange={handleChange}
+        onChange={handleAutocompleteChange}
         options={classifiedWordsList}
-        defaultValue={[
-          classifiedWordsList[4],
-          classifiedWordsList[10],
-          classifiedWordsList[15],
-          classifiedWordsList[20],
-          classifiedWordsList[25],
-          classifiedWordsList[33],
-          classifiedWordsList[22],
-          classifiedWordsList[29],
-        ]}
+        defaultValue={defaultWords}
         disableCloseOnSelect
         getOptionLabel={(option) => {
           if (typeof option === "string") {
             return option;
           }
-          if (option.word) {
-            return option.word;
+          if (option) {
+            return option;
           }
-          return option.word;
+          return option;
         }}
         filterOptions={(options, params) => {
           const filtered = filter(options, params);
 
           if (params.inputValue !== "") {
-            filtered.push({
-              word: params.inputValue,
-            });
+            filtered.push(params.inputValue);
           }
 
           return filtered;
@@ -85,7 +80,7 @@ const ClassifiedTool = () => {
               style={{ marginRight: 8 }}
               checked={selected}
             />
-            {option.word}
+            {option}
           </li>
         )}
         renderInput={(params) => (
@@ -96,60 +91,113 @@ const ClassifiedTool = () => {
           />
         )}
       />
+      {result[1] && (
+        <div style={{ marginTop: "10px" }}>
+          <Typography variant="subtitle1" color="primary" gutterBottom>
+            Checked Email Text: {result[1]}
+          </Typography>
+
+          <Typography variant="subtitle1" color="primary" gutterBottom>
+            Classified Words: {result[0] ? "Yes" : "No"}
+          </Typography>
+        </div>
+      )}
     </React.Fragment>
   );
 };
 
 export default ClassifiedTool;
 
+// TODO: move this logic to the backend
+const isClassified = (emailText, selectedOptions) => {
+  // if the email is empty, then return false
+  if (emailText === "") {
+    return [false, emailText];
+  }
+
+  // if the selectedOptions is empty, then return false
+  if (selectedOptions.length === 0) {
+    return [false, emailText];
+  }
+
+  // if the email contains any of the selectedOptions, then return true as well as the email with the classified words replaced with asterisks
+  for (let i = 0; i < selectedOptions.length; i++) {
+    if (emailText.includes(selectedOptions[i])) {
+      // TODO: optimize this function if we're processing a large amount of emails
+      return [
+        true,
+        emailText.replace(
+          selectedOptions[i],
+          "*".repeat(selectedOptions[i].length)
+        ),
+      ];
+    }
+  }
+
+  // if the email doesn't contain any of the selectedOptions, then return false
+  return [false, emailText];
+};
+
+// TODO: move this to a database; generate alternative words based on these words
 const classifiedWordsList = [
-  { word: "Abu Omar case" },
-  { word: "Acoustic Kitty" },
-  { word: "Air Bridge Denial Program" },
-  { word: "MS Alfhem" },
-  { word: "American Committee on United Europe" },
-  { word: "SS Aquila (1940)" },
-  { word: "SS Armonia (1924)" },
-  { word: "Project ARTICHOKE" },
-  { word: "Assassination attempts on Fidel Castro" },
-  { word: "Battalion 3-16 (Honduras)" },
-  { word: "Bay of Pigs Invasion" },
-  { word: "William H. Beale" },
-  { word: "Darrell Blocker" },
-  { word: "Canadian Caper" },
-  { word: "Operation CHAOS" },
-  { word: "1973 Chilean coup d'état" },
-  { word: "Church Committee" },
-  { word: "CIA black sites" },
-  { word: "Allegations of CIA drug trafficking" },
-  { word: "CIA Tibetan program" },
-  { word: "International counter-terroism activities of the CIA" },
-  { word: "Project COLDFEET" },
-  { word: "Contras" },
-  { word: "Operation Easy Chair" },
-  { word: "MV Daronia" },
-  { word: "Disposition Matrix" },
-  { word: "Edgewood Arsenal human experiments" },
-  { word: "Executive actions of the CIA" },
-  { word: "Extraordinary rendition" },
-  { word: "Extraordinary repatriation" },
-  { word: "Farewell Dossier" },
-  { word: "SS Flying Lark" },
-  { word: "Project FUBELT" },
-  { word: "Operation Gold" },
-  { word: "Greystone (CIA operation)" },
-  { word: "Habbush letter" },
-  { word: "HTLINGUAL" },
-  { word: "Human Ecology Fund" },
-  { word: "1953 Iranian coup d'état" },
-  { word: "1989 Angola Lockheed L-100 crash" },
-  { word: "Project Azorian" },
-  { word: "JMWAVE" },
-  { word: "Joint Advisory Commission, Korea" },
-  { word: "J. C. King" },
-  { word: "Battle of Luang Namtha" },
-  { word: "Khalid El-Masri" },
-  { word: "Measurements of Earth Data for Environmental Analysis" },
-  { word: "Operation Merlin" },
-  { word: "Project MERRIMAC" },
+  "Abu Omar case",
+  "Acoustic Kitty",
+  "Air Bridge Denial Program",
+  "MS Alfhem",
+  "American Committee on United Europe",
+  "SS Aquila (1940)",
+  "SS Armonia (1924)",
+  "Project ARTICHOKE",
+  "Assassination attempts on Fidel Castro",
+  "Battalion 3-16 (Honduras)",
+  "Bay of Pigs Invasion",
+  "William H. Beale",
+  "Darrell Blocker",
+  "Canadian Caper",
+  "Operation CHAOS",
+  "1973 Chilean coup d'état",
+  "Church Committee",
+  "CIA black sites",
+  "Allegations of CIA drug trafficking",
+  "CIA Tibetan program",
+  "International counter-terroism activities of the CIA",
+  "Project COLDFEET",
+  "Contras",
+  "Operation Easy Chair",
+  "MV Daronia",
+  "Disposition Matrix",
+  "Edgewood Arsenal human experiments",
+  "Executive actions of the CIA",
+  "Extraordinary rendition",
+  "Extraordinary repatriation",
+  "Farewell Dossier",
+  "SS Flying Lark",
+  "Project FUBELT",
+  "Operation Gold",
+  "Greystone (CIA operation)",
+  "Habbush letter",
+  "HTLINGUAL",
+  "Human Ecology Fund",
+  "1953 Iranian coup d'état",
+  "1989 Angola Lockheed L-100 crash",
+  "Project Azorian",
+  "JMWAVE",
+  "Joint Advisory Commission, Korea",
+  "J. C. King",
+  "Battle of Luang Namtha",
+  "Khalid El-Masri",
+  "Measurements of Earth Data for Environmental Analysis",
+  "Operation Merlin",
+  "Project MERRIMAC",
+];
+
+const defaultWords = [
+  classifiedWordsList[4],
+  classifiedWordsList[10],
+  classifiedWordsList[15],
+  classifiedWordsList[20],
+  classifiedWordsList[25],
+  classifiedWordsList[33],
+  classifiedWordsList[22],
+  classifiedWordsList[29],
 ];
