069cf121cedb7beda731e720d2db2efe58bb8f20
Austin Schrader <austin.jschrader@gmail.com>
2022-04-21 17:15:12 -0700

    Revert footer styled component refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,66 @@
 import React from "react";
-import { styled } from "@mui/material/styles";
-
-const Column = styled("div", {
-  display: "flex",
-  justifyContent: "space-around",
-  flexDirection: "column",
-});
-
-const FooterContainer = styled("div", {
-  display: "flex",
-  justifyContent: "space-around",
-  alignItems: "center",
-  padding: "20px",
-  listStyle: "none",
-});
 
 const Footer = () => {
   return (
-    <FooterContainer>
-      <Column>
+    <div
+      className="footer"
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        padding: "20px",
+        listStyle: "none",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexDirection: "column",
+        }}
+      >
         <a href="/#">Home</a>
         <a href="/#">The Administration</a>
         <a href="/#">Executive Offices</a>
         <a href="/#">Priorities</a>
-      </Column>
-      <Column>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexDirection: "column",
+        }}
+      >
         <a href="/#">Briefing Room</a>
         <a href="/#">The White House</a>
         <a href="/#">Get Involved</a>
         <a href="/#">Español</a>
-      </Column>
-      <Column>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexDirection: "column",
+        }}
+      >
         <a href="/#">Contact Us</a>
         <a href="/#">Privacy Policy</a>
         <a href="/#">Copyright Policy</a>
         <a href="/#">Accessibility Statements</a>
         <a href="/#">Disclosures</a>
-      </Column>
-      <Column>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          flexDirection: "column",
+        }}
+      >
         <span style={{ fontWeight: "bold" }}>The White House</span>
         <span>1600 Pennsylvania Ave NW</span>
         <span>Washington, DC 20500</span>
-      </Column>
-    </FooterContainer>
+      </div>
+    </div>
   );
 };
 
